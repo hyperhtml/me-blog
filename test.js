493c0b23db2392e45ab791194e237324e704682a
Bryce Carr <hyperhtml@users.noreply.github.com>
2014-07-29 21:04:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 function onRequest(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hello World");
+  response.write("<h1>Hello World!</h1>");
   response.end();
 }
 
