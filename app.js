e1484bcf840d363633971fa641c05e4a6be2ca71
Bryce Carr <bryce@hyperhtml.net>
2015-02-02 20:20:56 -0500

    Added watchdog and posts updates

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var post = require('./routes/posts');
+
+var app = express()
+
+
+// Blog Entry List
+app.get('/', post.findAll);
+app.get('/post/:id', post.findById);
+
+var server = app.listen(80, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
