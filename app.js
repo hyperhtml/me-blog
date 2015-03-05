15b45c0e3b99c6c297761dde589f1291233b170d
Bryce Carr <bryce@hyperhtml.net>
2015-03-04 21:05:54 -0500

    Added Google Auth

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ var exphbs  = require('express-handlebars');
 
 var app = express()
 
+var passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://brycecarr.me/auth/google/return',
+    realm: 'http://brycecarr.me/'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+      console.log('user', user)
+    });
+    console.log("Auth",identifier, profile, done);
+  }
+));
+
 // Setup Handlebars Templating
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -11,6 +27,16 @@ app.set('view engine', 'handlebars');
 // Blog Entry List
 app.get('/', post.findAll);
 app.get('/post/:id', post.findById);
+
+/* Google Auth */
+app.get('/auth/google', passport.authenticate('google'));
+
+app.get('/auth/google/return',
+  passport.authenticate('google', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+  }));
+
 
 var server = app.listen(80, function () {
 
